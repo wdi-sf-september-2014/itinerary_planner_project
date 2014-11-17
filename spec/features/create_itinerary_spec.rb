b1289d9fb76609cb4eda6dec4b10e688ec760f40
Alex Notov <anotov@gmail.com>
2014-11-16 20:43:16 -0800

    Add exercise step; fix comment issues in spec

--- spec/features/create_itinerary_spec.rb
+++ spec/features/create_itinerary_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 feature "Creating an Itinerary", :type => :feature do
   scenario "with valid parameters" do
     #1. setup
+    #2. exercise
     visit new_itinerary_path
+    fill_in "Name", with: "Trip to Paris"
+    fill_in "Origin", with: "San Francisco, CA"
+    fill_in "Destination", with: "Paris, FR"
+    #3. verify
   end
 end

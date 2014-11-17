646e796edef2f7fb18a79ac6cb29fe3415f56a28
Alex Notov <anotov@gmail.com>
2014-11-16 19:51:35 -0800

    Fix rails helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -3,6 +3,11 @@ ENV["RAILS_ENV"] ||= 'test'
 require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
+require 'capybara/rails'
+require 'capybara/rspec'
+require 'rspec/rails'
+require 'shoulda/matchers'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -27,6 +32,8 @@ ActiveRecord::Migration.check_pending!
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  config.include FactoryGirl::Syntax::Methods
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
